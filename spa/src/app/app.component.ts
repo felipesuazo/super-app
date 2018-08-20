d4ba0796370c8cc95eadbfb984e88cd81eac420b
Felipe Suazo <felipe.suazom@gmail.com>
2018-08-20 16:54:20 -0300

    Added notifications as ngrx store effects

--- spa/src/app/app.component.ts
+++ spa/src/app/app.component.ts
@@ -2,33 +2,19 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {NzNotificationService} from 'ng-zorro-antd';
 import {AppState} from './reducers';
-import {Store, select} from '@ngrx/store';
+import {Store, select } from '@ngrx/store';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   isCollapsed = false;
 
-  constructor(private router: Router,
-              private notification: NzNotificationService,
-              private store: Store<AppState>) {}
-
-  ngOnInit() {
-    this.store.pipe(select(state => state.userState.failed.save)).subscribe((failed) => {
-      if (failed) {
-        this.createNotification('error', 'Error', 'Cannot add user.');
-      }
-    });
-  }
+  constructor(private router: Router) {}
 
   routeIsActive(route: string) {
     return this.router.url === route;
-  }
-
-  createNotification(type: string, title: string, message: string) {
-    this.notification.create(type, title, message);
   }
 }
--- spa/src/app/reducers/user.effect.ts
+++ spa/src/app/reducers/user.effect.ts
@@ -3,13 +3,23 @@ import {Actions, Effect, ofType} from '@ngrx/effects';
 import {Action} from '@ngrx/store';
 import {ApiService} from '../services/api.service';
 import {Observable, of} from 'rxjs';
-import {FETCH_USERS, FETCH_USERS_SUCCESS, SAVE_USER, SAVE_USER_SUCCESS, SaveUser, SaveUserFailed} from '../actions/user.action';
+import {
+  FETCH_USERS,
+  FETCH_USERS_SUCCESS,
+  SAVE_USER,
+  SAVE_USER_FAILED,
+  SAVE_USER_SUCCESS,
+  SaveUser,
+  SaveUserFailed, SaveUserSuccess
+} from '../actions/user.action';
 import {catchError, map, mergeMap} from 'rxjs/operators';
+import {NzNotificationService} from 'ng-zorro-antd';
 
 @Injectable()
 export class UserEffect {
   constructor(private actions$: Actions,
-              private api: ApiService) {}
+              private api: ApiService,
+              private notification: NzNotificationService) {}
 
   @Effect()
   fetchUsers$: Observable<Action> = this.actions$.pipe(
@@ -31,4 +41,28 @@ export class UserEffect {
       )
     )
   );
+
+  @Effect({dispatch: false})
+  saveUserFailed$ = this.actions$.pipe(
+    ofType(SAVE_USER_FAILED),
+    mergeMap(() => {
+      this.createNotification('error', 'Error', 'Cannot add user');
+      return null;
+    })
+  );
+
+  @Effect({dispatch: false})
+  saveUserSuccess$ = this.actions$.pipe(
+    ofType(SAVE_USER_SUCCESS),
+    mergeMap((action: SaveUserSuccess) => {
+      this.createNotification('success', 'Success', `User ${action.payload.name} added succesfully`);
+      return null;
+    })
+  );
+
+
+
+  createNotification(type: string, title: string, message: string) {
+    this.notification.create(type, title, message);
+  }
 }
